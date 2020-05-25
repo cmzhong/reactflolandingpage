a610906cbb480c3b5746e05014683531ca791b82
jdele <jordanscottdeleon@gmail.com>
2020-05-25 15:18:55 -0700

    added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,18 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `./images/logo.png`,
+      },
+    },
+  ],
 }
